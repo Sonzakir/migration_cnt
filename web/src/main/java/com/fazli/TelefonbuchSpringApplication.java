098c50751dfab06c186dc7000f8ef42b961e9eed
Sonzakir <sonerkiraz8@gmail.com>
2024-12-19 17:51:49 +0100

    Initial commit

---
+++ web/src/main/java/com/fazli/TelefonbuchSpringApplication.java
@@ -0,0 +1,15 @@
+package com.fazli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelefonbuchSpringApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelefonbuchSpringApplication.class, args);
+    }
+
+}
