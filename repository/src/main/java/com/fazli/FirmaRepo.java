e7e33637aed8124c2ca624074ad6ec4fc4b3ff8a
Sonzakir <sonerkiraz8@gmail.com>
2024-12-19 19:30:17 +0100

    backup W

--- repository/src/main/java/com/fazli/FirmaRepo.java
+++ repository/src/main/java/com/fazli/FirmaRepo.java
@@ -4,9 +4,10 @@ package com.fazli;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface FirmaRepo extends JpaRepository<Firma, Long> {
 
 
--- repository/src/main/java/com/fazli/PersonRepo.java
+++ repository/src/main/java/com/fazli/PersonRepo.java
@@ -3,9 +3,11 @@ package com.fazli;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PersonRepo extends JpaRepository<Person, Long> {
 
 
